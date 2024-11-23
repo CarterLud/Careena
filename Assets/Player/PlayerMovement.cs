43bf5b9f3c03e8af3d518e915e1b399b5373c6dd
MeenaRam23 <meenaram2030@gmail.com>
2024-11-23 00:01:48 -0800

    Player movement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
    // private Vector2 playerDirection;
-  //  public float playerSpeed;
+    public float speed;
 
     public Rigidbody2D body;
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //playerDirection = new Vector2();
-       // playerDirection.x = Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(1, 0);
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        if(Mathf.Abs(xInput) > 0)
+        {
+            body.velocity = new Vector2(xInput * speed, body.velocity.y);
+        }
+        if(Mathf.Abs(yInput)>0)
+        {
+            body.velocity = new Vector2(body.velocity.x, yInput * speed);
+        }
+        Vector2 direction= new Vector2(xInput, yInput).normalized;
+
+        body.velocity = direction * speed;
+       
     }
 }
